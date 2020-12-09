330942bbe801dc58fcaaa8856967bb4aac0e0fa9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-12-09 12:49:47 +0100

    added examples
    added examples

--- ocel/__init__.py
+++ ocel/__init__.py
@@ -6,3 +6,15 @@ __author__ = 'PADS'
 __author_email__ = 'a.berti@pads.rwth-aachen.de'
 __maintainer__ = 'PADS'
 __maintainer_email__ = "a.berti@pads.rwth-aachen.de"
+
+
+def import_log(log_path):
+    return importer.importer.apply(log_path)
+
+
+def export_log(log, log_path):
+    return exporter.exporter.apply(log, log_path)
+
+
+def validate(log_path, schema_path):
+    return validation.validate.apply(log_path, schema_path)
