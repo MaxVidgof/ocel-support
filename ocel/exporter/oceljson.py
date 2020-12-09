53e8520948d396c642ffbd688a5249c16cd25169
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-12-09 11:57:12 +0100

    intermediate commit

--- ocel/exporter/oceljson.py
+++ ocel/exporter/oceljson.py
@@ -1,8 +1,13 @@
 import json
+import datetime
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 
 def apply(log, output_path, parameters=None):
     if parameters is None:
         parameters = {}
 
-    json.dump(log, open(output_path, "w"), indent=2)
+    json.dump(log, open(output_path, "w"), indent=2, default=myconverter)
